9aff73a0e5f38f43273d4cbdf6e2119169686db2
nodox <snatera09@gmail.com>
2017-02-16 17:52:53 -0600

    build(*): Scaffold initial project configurations

---
+++ test/server/api.users.spec.js
@@ -0,0 +1,27 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+
+// Our app server
+let server = require('../../src/app');
+
+// Configuration
+chai.use(chaiHttp);
+
+
+
+describe('GET /users', function() {
+
+  it('should return 200', function() {
+    chai.request(server)
+      .get('/users')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+});
+
+
+     
