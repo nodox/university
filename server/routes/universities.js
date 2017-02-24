5d43280b3c101711e41d17bbe9515daacc551628
carter-d <carterdurno@gmail.com>
2017-02-24 10:24:51 -0600

    started database testing

--- server/routes/universities.js
+++ server/routes/universities.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	//var conString = "postgres://notpfdixvpwqgg:5d9e16ff60f11b3594da8e7ccd010e843629a9ddb7976fa1519c611223c70ec8@ec2-54-163-234-140.compute-1.amazonaws.com:5432/d3nu6migmun6tt"+"?ssl=true";
   var conString = process.env.DATABASE_URL + "?ssl=true";
 	var client = new pg.Client(conString);
   pg.connect(conString, function(err, client, done) {
