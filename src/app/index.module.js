81ce50c7bd2fbd3f18fd47efa0ec9b3afff1ef56
nodox <snatera09@gmail.com>
2017-02-24 11:44:40 -0600

    feat(frontend): Configure express to serve the angular app

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,6 +4,11 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
+import { DocsController } from './docs/docs.controller';
+import { TutorialController } from './tutorial/tutorial.controller';
+import { DemoController } from './demo/demo.controller';
+
+
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
@@ -18,5 +23,8 @@ angular.module('universityClient', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSani
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
+  .controller('DocsController', MainController)
+  .controller('TutorialController', MainController)
+  .controller('DemoController', MainController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,6 +6,24 @@ export function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+    .state('tutorial', {
+      url: '/tutorial',
+      templateUrl: 'app/tutorial/tutorial.html',
+      controller: 'TutorialController',
+      controllerAs: 'tutorial'
+    })
+    .state('demo', {
+      url: '/demo',
+      templateUrl: 'app/demo/demo.html',
+      controller: 'DemoController',
+      controllerAs: 'demo'
+    })
+    .state('docs', {
+      url: '/docs',
+      templateUrl: 'app/docs/docs.html',
+      controller: 'DocsController',
+      controllerAs: 'docs'
     });
 
   $urlRouterProvider.otherwise('/');
