c8a1450dc144162ddc9878652ca606d2ec688f33
cobuma <marquardt.corey@gmail.com>
2017-02-27 19:03:41 -0600

    update placeholder url for conString

--- server/routes/course-queries.js
+++ server/routes/course-queries.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 //  update conString once courses database is created
-var conString = "postgres://notpfdixvpwqgg:5d9e16ff60f11b3594da8e7ccd010e843629a9ddb7976fa1519c611223c70ec8@ec2-54-163-234-140.compute-1.amazonaws.com:5432/d3nu6migmun6tt"+"?ssl=true";
+var conString = process.env.DATABASE_URL + "?ssl=true";
 var client = pg.Client(conString);
 
 function getAllCourses(req, res, next) {
