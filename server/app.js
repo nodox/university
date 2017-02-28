1ab1eeb4861f6960e320d4f4d3e97e132635d791
cobuma <marquardt.corey@gmail.com>
2017-02-27 18:50:36 -0600

    rename files

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ const index = require('./routes/index');
 const users = require('./routes/users');
 const courses = require('./routes/courses');
 const universities = require('./routes/universities');
-const university_queries = require('./routes/university_queries');
-const course_queries = require('./routes/course_queries');
+const university_queries = require('./routes/university-queries');
+const course_queries = require('./routes/course-queries');
 
 const app = express();
 
@@ -31,7 +31,6 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/universities', universities);
 app.use('/courses', courses);
-// app.use('/universities', university_id);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- server/routes/universities.js
+++ server/routes/universities.js
@@ -2,40 +2,9 @@ var express = require('express');
 var pg = require('pg');
 var router = express.Router();
 
-var db = require('./university_queries');
+var db = require('./university-queries');
 
 router.get('/', db.getAllUnis);
 router.get('/:id', db.getSingleUni)
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   // var conString = process.env.DATABASE_URL + "?ssl=true";
-//   var conString = "postgres://notpfdixvpwqgg:5d9e16ff60f11b3594da8e7ccd010e843629a9ddb7976fa1519c611223c70ec8@ec2-54-163-234-140.compute-1.amazonaws.com:5432/d3nu6migmun6tt"+"?ssl=true";
-// 	var client = new pg.Client(conString);
-//   pg.connect(conString, function(err, client, done) {
-//     client.query('SELECT * FROM university', function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); res.send("Error " + err); }
-//       else
-//        { res.json(result.rows); }
-//     });
-//   });
-// });
-
-// router.get('/:id', function(req, res, next) {
-//   // var conString = process.env.DATABASE_URL + "?ssl=true";
-//   var conString = "postgres://notpfdixvpwqgg:5d9e16ff60f11b3594da8e7ccd010e843629a9ddb7976fa1519c611223c70ec8@ec2-54-163-234-140.compute-1.amazonaws.com:5432/d3nu6migmun6tt"+"?ssl=true";
-//   var client = new pg.Client(conString);
-//   pg.connect(conString, function(err, client, done) {
-//     client.query('SELECT * FROM university where id = req.params.id', function(err, result) {
-//         done();
-//         if (err)
-//          { console.error(err); res.send("Error " + err); }
-//         else
-//          { res.json(result.rows); }
-//       });
-
-//   });
-// });
 
 module.exports = router;
