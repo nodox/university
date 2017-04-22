50d5830425c39102e2da2127f309cab990ee34f3
nodox <snatera09@gmail.com>
2017-04-22 01:00:34 +0000

    made some change in ubuntu production

--- importer.py
+++ importer.py
@@ -1,32 +1,32 @@
-# import csv
-# import requests
+import csv
+import requests
 
-# with open('engineering_courses.csv', 'rU') as csvfile:
+with open('engineering_courses.csv', 'rU') as csvfile:
 
-#     reader = csv.DictReader(csvfile, delimiter=',')
-#     count = 0
-#     for row in reader:
+     reader = csv.DictReader(csvfile, delimiter=',')
+     count = 0
+     for row in reader:
 
-#         try:
-#             course = {}
-#             course['title'] = row['title']
-#             course['description'] = row['description']
-#             course['collegeId'] = row['collegeId']
-#             course['building'] = row['building']
-#             course['code'] = row['code']
-#             course['days'] = row['days']
-#             course['credits'] = row['credits']
-#             course['time'] = row['time']
-#             course['instructor'] = row['instructor']
-#             course['department'] = row['department']
-#             course['school'] = row['school']
-#             print course
-#             r = requests.post('http://localhost:3000/course/', data=course)
-#             count += 1
+         try:
+             course = {}
+             course['title'] = row['title']
+             course['description'] = row['description']
+             course['collegeId'] = row['collegeId']
+             course['building'] = row['building']
+             course['code'] = row['code']
+             course['days'] = row['days']
+             course['credits'] = row['credits']
+             course['time'] = row['time']
+             course['instructor'] = row['instructor']
+             course['department'] = row['department']
+             course['school'] = row['school']
+             print course
+             r = requests.post('http://34.200.240.84:3000/course/', data=course)
+             count += 1
 
 
-#         except Exception as e:
-#             print e
+         except Exception as e:
+             print e
 
 
 
