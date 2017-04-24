4dd2a69674f9c3a218f3e192b0cf376fd0e37ce2
nodox <snatera09@gmail.com>
2017-04-24 12:24:40 -0500

    add cors for 3rd party apps

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 const index = require('./routes/index');
 const university = require('./routes/university');
 const course = require('./routes/course');
-
+const cors = require('cors');
 const app = express();
 
 // view engine setup
@@ -22,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use('/', index);
